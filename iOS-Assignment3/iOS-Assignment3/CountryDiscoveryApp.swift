00108c9f3584ef581042bba17fee9ff9d2db224d
OliviaLim123 <131115382+OliviaLim123@users.noreply.github.com>
2024-05-07 10:11:33 +1000

    Changing the CountryDiscoveryApp
    I changed the CountryDiscoveryApp file so that the first screen to be shown is the AppEntry :)

--- iOS-Assignment3/iOS-Assignment3/CountryDiscoveryApp.swift
+++ iOS-Assignment3/iOS-Assignment3/CountryDiscoveryApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct CountryDiscoveryApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            NavigationStack {
+                AppEntry()
+            }
         }
     }
 }
